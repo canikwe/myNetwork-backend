98c5ba82ba0f9b7c48b005a82177f7e124ab7919
canikwe <c.anikwe@gmail.com>
2019-04-16 16:09:00 -0400

    add timestamps to contact serializer

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,5 +1,5 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :first_name, :last_name, :friend_id, :kind, :details, :avatar
+  attributes :id, :name, :first_name, :last_name, :friend_id, :kind, :details, :avatar, :created_at, :updated_at
   
   belongs_to :requestor, foreign_key: "requestor_id", class_name: "User"
   belongs_to :requested, foreign_key: "requested_id", class_name: "User"
